7e5758851e7d8e378c4857d4f37fc7c079c44187
Siyi YanYuan <67218222+KamenRider41@users.noreply.github.com>
2023-03-09 11:07:33 +0800

    feat: 更新配置测试

--- config/index.cjs
+++ config/index.cjs
@@ -23,6 +23,8 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'SCT200199Tt5LHwk2fbquZfLL6ARN5IMQS',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
+      PROVINCE: '湖南',
+      CITY: '长沙',
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '3-24',
