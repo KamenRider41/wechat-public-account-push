8c805ca6a63d5b43b845d0f666e43f015f565087
Siyi YanYuan <67218222+KamenRider41@users.noreply.github.com>
2023-03-15 09:20:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,20 +13,20 @@ const USER_CONFIG = {
   PROVINCE: '湖南',
   CITY: '长沙',
   USERS: [
-    // {
-    //   // 想要发送的人的名字
-    //   name: '亲爱的41',
-    //   // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-    //   id: 'SCT200199Tt5LHwk2fbquZfLL6ARN5IMQS',
-    //   // 使用微信测试号：你想对他发送的模板消息的模板ID
-    //   PROVINCE: '湖南',
-    //   CITY: '长沙',
-    //   useTemplateId: '0001',
-    //   // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-    //   horoscopeDate: '3-24',
-    //   festivals: [
-    //   ],
-    // },
+    {
+      // 想要发送的人的名字
+      name: '亲爱的41',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'SCT200199Tt5LHwk2fbquZfLL6ARN5IMQS',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      PROVINCE: '湖南',
+      CITY: '长沙',
+      useTemplateId: '0001',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '3-24',
+      festivals: [
+      ],
+    },
     // {
     //   // 想要发送的人的名字
     //   name: '亲爱的轩哥',
@@ -39,36 +39,36 @@ const USER_CONFIG = {
     //   // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
     //   horoscopeDate: '3-24',
     // },
-    {
-      // 想要发送的人的名字
-      name: '亲爱的曾桑',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT200199Tt5LHwk2fbquZfLL6ARN5IMQS',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      PROVINCE: '湖南',
-      CITY: '长沙',
-      useTemplateId: '0001',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '3-24',
-      festivals: [
-        // // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        // {
-        //   type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        // },
-        // // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        // {
-        //   type: '生日', name: '李四', year: '1996', date: '09-31',
-        // },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '02-12',
-        },
-      ],
-      // // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 认识的日子
-        { keyword: 'meet_day', date: '2023-03-09' },
-      ],
-    },
+//     {
+//       // 想要发送的人的名字
+//       name: '亲爱的曾桑',
+//       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: 'SCT200199Tt5LHwk2fbquZfLL6ARN5IMQS',
+//       // 使用微信测试号：你想对他发送的模板消息的模板ID
+//       PROVINCE: '湖南',
+//       CITY: '长沙',
+//       useTemplateId: '0001',
+//       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+//       horoscopeDate: '3-24',
+//       festivals: [
+//         // // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         // {
+//         //   type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+//         // },
+//         // // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         // {
+//         //   type: '生日', name: '李四', year: '1996', date: '09-31',
+//         // },
+//         {
+//           type: '节日', name: '相识纪念日', year: '2023', date: '02-12',
+//         },
+//       ],
+//       // // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 认识的日子
+//         { keyword: 'meet_day', date: '2023-03-09' },
+//       ],
+//     },
   ],
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '0010',
